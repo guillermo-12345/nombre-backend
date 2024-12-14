c5c247b5e5c95e34b5a23b140afb46f9aca1ac1f
guillermo-12345 <guillermo.ibanezc@gmail.com>
2024-12-14 15:54:53 -0300

    vercel28

--- server.js
+++ server.js
@@ -16,22 +16,25 @@ require('dotenv').config();
 const app = express();
 
 // CORS configuration
-const allowedOrigins = [process.env.FRONTEND_URL, 'https://equipo1-ecommerce-nuevo.vercel.app'];
-
-// CORS configuration
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: process.env.FRONTEND_URL || 'https://equipo1-ecommerce-nuevo.vercel.app',
   credentials: true,
 }));
+
+// CORS configuration
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// }));
 
 // Handle preflight requests
 app.options('*', cors());
