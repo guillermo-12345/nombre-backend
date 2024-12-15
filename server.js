d308ba3967eb2a82ad2d6336bf4f9f5a5c37c60c
guillermo-12345 <guillermo.ibanezc@gmail.com>
2024-12-14 21:57:20 -0300

    vercel31

--- server.js
+++ server.js
@@ -17,28 +17,22 @@ const app = express();
 
 app.use(cors({
   origin: 'https://equipo1-ecommerce-nuevo.vercel.app',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
 }));
-
-
-app.use(bodyParser.json());
-
-// Add headers middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://equipo1-ecommerce-nuevo.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
-
 
 // Middleware
 app.use(express.json());
 app.use(bodyParser.json());
 
+// Debugging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request to ${req.url}`);
+  console.log('Origin:', req.get('Origin'));
+  console.log('Headers:', req.headers);
+  next();
+});
 
 // Rutas de autenticación
 app.use('/auth', authRoutes);
